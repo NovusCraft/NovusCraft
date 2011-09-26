b5d6e83c3e82a920324e35ebbf62675f2080cfee
Arnold Zokas <arnold.zokas@coderoom.net>
2011-09-26 14:29:17 +0100

    Created and configured GlobalAssemblyInfo

---
+++ source/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#elif RELEASE
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: AssemblyCompany("Novus Craft")]
+[assembly: AssemblyCopyright("Copyright © Novus Craft 2011")]
+[assembly: AssemblyProduct("Novus Craft Website")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
