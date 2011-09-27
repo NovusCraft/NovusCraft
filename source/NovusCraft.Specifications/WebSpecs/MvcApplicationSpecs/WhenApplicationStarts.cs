bf83fa6e7855a2a0b2728200d0d55db2c761afc7
Arnold Zokas <arnold.zokas@coderoom.net>
2011-09-27 15:07:24 +0100

    Minor refactoring

--- source/NovusCraft.Specifications/WebSpecs/MvcApplicationSpecs/WhenApplicationStarts.cs
+++ source/NovusCraft.Specifications/WebSpecs/MvcApplicationSpecs/WhenApplicationStarts.cs
@@ -8,8 +8,7 @@ namespace NovusCraft.Specifications.WebSpecs.MvcApplicationSpecs
 	[Subject(typeof (MvcApplication))]
 	public class when_application_starts : MvcApplicationSpec
 	{
-		Because of =
-			() => Application.Application_Start();
+		Because of = () => Application.Application_Start();
 
 		It should_add_handle_error_filter_to_global_filters =
 			() => GlobalFilters.Filters.ShouldContain(f => f.Instance.GetType().Name == typeof (HandleErrorAttribute).Name);
