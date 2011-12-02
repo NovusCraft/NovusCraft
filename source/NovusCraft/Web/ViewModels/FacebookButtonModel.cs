4f62a0c85707a702401ac8138c59df8d53c2811c
ArnoldZokas <arnold.zokas@coderoom.net>
2011-12-02 22:06:19 +0000

    Implemented Facebook button

---
+++ source/NovusCraft/Web/ViewModels/FacebookButtonModel.cs
@@ -0,0 +1,10 @@
+﻿// # Copyright © 2011, Novus Craft
+// # All rights reserved. 
+
+namespace NovusCraft.Web.ViewModels
+{
+	public sealed class FacebookButtonModel
+	{
+		public string Action { get; set; }
+	}
+}
