a8813bc0dabb346a2e22cb54c0a4a7b93558994d
ArnoldZokas <arnold.zokas@coderoom.net>
2011-12-06 15:50:43 +0000

    Implemented Tweet button

---
+++ source/NovusCraft/Web/ViewModels/TwitterButtonModel.cs
@@ -0,0 +1,10 @@
+﻿// # Copyright © 2011, Novus Craft
+// # All rights reserved. 
+
+namespace NovusCraft.Web.ViewModels
+{
+	public sealed class TwitterButtonModel
+	{
+		public string Text { get; set; }
+	}
+}
