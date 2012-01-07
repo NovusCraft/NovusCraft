5cac2f2c26e2d6460c0d30f236b601baa2ec7583
ArnoldZokas <arnold.zokas@coderoom.net>
2012-01-07 00:49:51 +0000

    Added spec to verify login

--- source/NovusCraft.Specifications/WebSpecs/ControllerSpecs/AccountControllerSpecs/when_user_logs_in.cs
+++ source/NovusCraft.Specifications/WebSpecs/ControllerSpecs/AccountControllerSpecs/when_user_logs_in.cs
@@ -5,9 +5,11 @@ using System.Reflection;
 using System.Web.Mvc;
 using Machine.Specifications;
 using Machine.Specifications.Mvc;
+using Moq;
 using NovusCraft.Specifications.Utils;
 using NovusCraft.Web.Controllers;
 using NovusCraft.Web.ViewModels;
+using It = Machine.Specifications.It;
 
 namespace NovusCraft.Specifications.WebSpecs.ControllerSpecs.AccountControllerSpecs
 {
@@ -15,12 +17,16 @@ namespace NovusCraft.Specifications.WebSpecs.ControllerSpecs.AccountControllerSp
 	public class when_user_logs_in : account_controller_spec
 	{
 		static ActionResult result;
+		static readonly LogInDetails log_in_details = new LogInDetails { Email = "example@company.com", Password = "password" };
 
 		Because of = () =>
 			{
 				account_management_service.Setup(ams => ams.LogIn(Moq.It.IsAny<LogInDetails>())).Returns(true);
-				result = controller.LogIn(new LogInDetails { Email = "example@company.com", Password = "password" });
+				result = controller.LogIn(log_in_details);
 			};
+
+		It should_log_user_in =
+			() => account_management_service.Verify(ams => ams.LogIn(log_in_details), Times.Exactly(1));
 
 		It should_display_dashboard_page =
 			() => result.ShouldBeARedirectToRoute().And().ActionName().ShouldEqual("Dashboard");
