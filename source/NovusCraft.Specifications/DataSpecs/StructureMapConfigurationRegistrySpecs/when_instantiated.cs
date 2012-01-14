1a7069fd20527b834b83d06b7c6067317ac741f2
ArnoldZokas <arnold.zokas@coderoom.net>
2012-01-14 01:28:13 +0000

    Updated StructureMapConfigurationRegistry to handle new security infrastructure

--- source/NovusCraft.Specifications/DataSpecs/StructureMapConfigurationRegistrySpecs/when_instantiated.cs
+++ source/NovusCraft.Specifications/DataSpecs/StructureMapConfigurationRegistrySpecs/when_instantiated.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Machine.Specifications;
 using NovusCraft.Data;
 using NovusCraft.Data.Blog;
+using NovusCraft.Data.Security;
+using NovusCraft.Security;
 using Raven.Client;
 using Raven.Client.Embedded;
 using StructureMap;
@@ -16,12 +18,7 @@ namespace NovusCraft.Specifications.DataSpecs.StructureMapConfigurationRegistryS
 	{
 		Because of = () => container = new Container(registry);
 
-		It should_register_blog_category_repository =
-			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IBlogPostRepository) && pt.Default.ConcreteType == typeof(BlogPostRepository)).ShouldNotBeNull();
-
-		It should_set_blog_category_repository_lifecycle_as_hybrid =
-			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IBlogPostRepository) && pt.Default.ConcreteType == typeof(BlogPostRepository)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
-
+		// Raven DB
 		It should_register_ravendb_document_store =
 			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IDocumentStore) && pt.Default.ConcreteType == typeof(EmbeddableDocumentStore)).ShouldNotBeNull();
 
@@ -33,5 +30,31 @@ namespace NovusCraft.Specifications.DataSpecs.StructureMapConfigurationRegistryS
 
 		It should_set_ravendb_document_session_lifecycle_as_hybrid =
 			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IDocumentSession) && pt.Default.Description == "Instance is created by Func<object> function:  System.Func`2[StructureMap.IContext,Raven.Client.IDocumentSession]").Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
+
+		// Blog
+		It should_register_blog_category_repository =
+			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IBlogPostRepository) && pt.Default.ConcreteType == typeof(BlogPostRepository)).ShouldNotBeNull();
+
+		It should_set_blog_category_repository_lifecycle_as_hybrid =
+			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IBlogPostRepository) && pt.Default.ConcreteType == typeof(BlogPostRepository)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
+
+		// Security
+		It should_register_user_account_repository =
+			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IUserAccountRepository) && pt.Default.ConcreteType == typeof(UserAccountRepository)).ShouldNotBeNull();
+
+		It should_set_user_account_repository_lifecycle_as_hybrid =
+			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IUserAccountRepository) && pt.Default.ConcreteType == typeof(UserAccountRepository)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
+
+		It should_register_forms_authentication_wrapper =
+			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IFormsAuthenticationWrapper) && pt.Default.ConcreteType == typeof(FormsAuthenticationWrapper)).ShouldNotBeNull();
+
+		It should_set_forms_authentication_wrapper_lifecycle_as_hybrid =
+			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IFormsAuthenticationWrapper) && pt.Default.ConcreteType == typeof(FormsAuthenticationWrapper)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
+
+		It should_register_authentication_service =
+			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IAuthenticationService) && pt.Default.ConcreteType == typeof(AuthenticationService)).ShouldNotBeNull();
+
+		It should_set_authentication_service_lifecycle_as_hybrid =
+			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IAuthenticationService) && pt.Default.ConcreteType == typeof(AuthenticationService)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
 	}
 }
--- source/NovusCraft/Data/StructureMapConfigurationRegistry.cs
+++ source/NovusCraft/Data/StructureMapConfigurationRegistry.cs
@@ -3,6 +3,8 @@
 
 using System;
 using NovusCraft.Data.Blog;
+using NovusCraft.Data.Security;
+using NovusCraft.Security;
 using Raven.Client;
 using StructureMap.Configuration.DSL;
 
@@ -13,9 +15,16 @@ namespace NovusCraft.Data
 	{
 		public StructureMapConfigurationRegistry()
 		{
+			// Raven DB
 			RegisterRavenDb();
 
+			// Blog
 			For<IBlogPostRepository>().HybridHttpOrThreadLocalScoped().Use<BlogPostRepository>();
+
+			// Security
+			For<IUserAccountRepository>().HybridHttpOrThreadLocalScoped().Use<UserAccountRepository>();
+			For<IFormsAuthenticationWrapper>().HybridHttpOrThreadLocalScoped().Use<FormsAuthenticationWrapper>();
+			For<IAuthenticationService>().HybridHttpOrThreadLocalScoped().Use<AuthenticationService>();
 		}
 
 		void RegisterRavenDb()
