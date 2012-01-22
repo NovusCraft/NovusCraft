4c3c235fcd5ff4648cb03353d27f4a88fa332bcb
ArnoldZokas <arnold.zokas@coderoom.net>
2012-01-22 23:50:57 +0000

    Refactored StructureMapConfigurationRegistry specs

--- source/NovusCraft.Specifications/DataSpecs/StructureMapConfigurationRegistrySpecs/when_instantiated.cs
+++ source/NovusCraft.Specifications/DataSpecs/StructureMapConfigurationRegistrySpecs/when_instantiated.cs
@@ -17,23 +17,14 @@ namespace NovusCraft.Specifications.DataSpecs.StructureMapConfigurationRegistryS
 		Because of = () => container = new Container(registry);
 
 		// Raven DB
-		It should_register_ravendb_document_store =
-			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IDocumentStore) && pt.Default.ConcreteType == typeof(EmbeddableDocumentStore)).ShouldNotBeNull();
-
-		It should_set_ravendb_document_store_lifecycle_as_singleton =
+		It should_register_singleton_ravendb_document_store =
 			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IDocumentStore) && pt.Default.ConcreteType == typeof(EmbeddableDocumentStore)).Lifecycle.ShouldEqual(InstanceScope.Singleton.ToString());
 
-		It should_register_ravendb_document_session =
-			() => container.GetInstance<IDocumentSession>().ShouldNotBeNull();
-
-		It should_set_ravendb_document_session_lifecycle_as_hybrid =
+		It should_register_hybrid_ravendb_document_session =
 			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IDocumentSession) && pt.Default.Description == "Instance is created by Func<object> function:  System.Func`2[StructureMap.IContext,Raven.Client.IDocumentSession]").Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
 
 		// Security
-		It should_register_forms_authentication_wrapper =
-			() => container.Model.PluginTypes.SingleOrDefault(pt => pt.PluginType == typeof(IFormsAuthenticationWrapper) && pt.Default.ConcreteType == typeof(FormsAuthenticationWrapper)).ShouldNotBeNull();
-
-		It should_set_forms_authentication_wrapper_lifecycle_as_hybrid =
+		It should_register_hybrind_forms_authentication_wrapper =
 			() => container.Model.PluginTypes.Single(pt => pt.PluginType == typeof(IFormsAuthenticationWrapper) && pt.Default.ConcreteType == typeof(FormsAuthenticationWrapper)).Lifecycle.ShouldEqual(InstanceScope.Hybrid.ToString());
 	}
 }
