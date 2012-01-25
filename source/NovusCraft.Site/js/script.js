3b20ac2946b8c49b9864cd61972bd1e3085addd8
ArnoldZokas <arnold.zokas@coderoom.net>
2012-01-25 01:11:51 +0000

    Implemented live preview for new blog post form

--- source/NovusCraft.Site/js/script.js
+++ source/NovusCraft.Site/js/script.js
@@ -1,4 +1,27 @@
 /* Author: Arnold Zokas */
-$(document).ready(function () {
+$(document).ready(function() {
 	$('pre code').each(function(i, e) { hljs.highlightBlock(e, '    '); });
+
+	$('#blog-post-editor').each(function() { var editor = new BlogPostEditor(); });
+});
+
+var BlogPostEditor = Backbone.View.extend({
+	el: "#blog-post-editor",
+	initialize: function () {
+		this.titleInput = $(this.el).find(".title");
+		this.titlePreview = $(this.el).find(".titlePreview");
+
+		this.contentInput = $(this.el).find(".content");
+		this.contentPreview = $(this.el).find(".contentPreview");
+	},
+	events: {
+		"keyup .title": "titleChanged",
+		"keyup .content": "contentChanged"
+	},
+	titleChanged: function () {
+		this.titlePreview.text(this.titleInput.val());
+	},
+	contentChanged: function () {
+		this.contentPreview.html(this.contentInput.val());
+	}
 });
