efe08a323c8d7284b7d2b6f852d3e21bc695bee5
ArnoldZokas <arnold.zokas@coderoom.net>
2012-01-26 14:45:44 +0000

    Marker all commands as DebuggerStepThrough

--- source/NovusCraft/Data/Blog/AddBlogPostCommand.cs
+++ source/NovusCraft/Data/Blog/AddBlogPostCommand.cs
@@ -1,10 +1,12 @@
 ﻿// # Copyright © 2011, Novus Craft
 // # All rights reserved. 
 
+using System.Diagnostics;
 using NovusCraft.Web.ViewModels;
 
 namespace NovusCraft.Data.Blog
 {
+	[DebuggerStepThrough]
 	public sealed class AddBlogPostCommand : Command<CreateBlogPostModel>
 	{
 		public AddBlogPostCommand(CreateBlogPostModel model) : base(model)
--- source/NovusCraft/Data/Blog/DeleteBlogPostCommand.cs
+++ source/NovusCraft/Data/Blog/DeleteBlogPostCommand.cs
@@ -1,8 +1,11 @@
 ﻿// # Copyright © 2011, Novus Craft
 // # All rights reserved. 
 
+using System.Diagnostics;
+
 namespace NovusCraft.Data.Blog
 {
+	[DebuggerStepThrough]
 	public sealed class DeleteBlogPostCommand : Command<int>
 	{
 		public DeleteBlogPostCommand(int model) : base(model)
